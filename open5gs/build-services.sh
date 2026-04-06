fab19a15f750ea2afe80b2d20a7602a21579d846
rkolakowski <rokolak@gmail.com>
2026-04-06 12:36:51 +0200

    fix: add bash script for service build

--- open5gs/build-services.sh
+++ open5gs/build-services.sh
@@ -4,7 +4,16 @@
 
 set -e
 
-KUSTOMIZE_DIR="${1:-.}"
+NAMESPACE="${1:-open5gs}"
 
-kustomize build "$KUSTOMIZE_DIR" \
-  | yq 'select(.kind == "Service")'
+kubectl apply -f common/amf/amf-service.yaml -n "$NAMESPACE"
+kubectl apply -f common/ausf/ausf-service.yaml -n "$NAMESPACE"
+kubectl apply -f common/nrf/nrf-service.yaml -n "$NAMESPACE"
+kubectl apply -f common/scp/scp-service.yaml -n "$NAMESPACE"
+kubectl apply -f common/nssf/nssf-service.yaml -n "$NAMESPACE"
+kubectl apply -f common/pcf/pcf-service.yaml -n "$NAMESPACE"
+kubectl apply -f common/udr/udr-service.yaml -n "$NAMESPACE"
+kubectl apply -f common/udm/udm-service.yaml -n "$NAMESPACE"
+kubectl apply -f common/bsf/bsf-service.yaml -n "$NAMESPACE"
+kubectl apply -f slices/slice1/smf1/smf-service.yaml -n "$NAMESPACE"
+kubectl apply -f slices/slice2/smf2/smf-service.yaml -n "$NAMESPACE"
